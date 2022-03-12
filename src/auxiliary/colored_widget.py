56c743d73570ab67e13d97b1050a3d2fde4ab42f
Pablo Sánchez Rodríguez <pablo@sanchezrodriguez.me>
2022-03-12 12:34:43 +0100

    Move colored_widget to auxiliary directory

--- src/auxiliary/colored_widget.py
+++ src/auxiliary/colored_widget.py
@@ -18,7 +18,7 @@
 from gi.repository import Adw, Gtk
 
 
-@Gtk.Template(resource_path='/me/sanchezrodriguez/passes/colored_widget.ui')
+@Gtk.Template(resource_path='/me/sanchezrodriguez/passes/auxiliary/colored_widget.ui')
 class ColoredWidget(Gtk.Box):
 
     __gtype_name__ = 'ColoredWidget'
