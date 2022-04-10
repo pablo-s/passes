85166e3de4578c730730c884990968c3f151ebc7
Pablo Sánchez Rodríguez <pablo@sanchezrodriguez.me>
2022-04-10 19:08:06 +0200

    Release 0.3

--- src/window.py
+++ src/window.py
@@ -143,7 +143,7 @@ class AboutDialog(Gtk.AboutDialog):
     def __init__(self, parent):
         Gtk.AboutDialog.__init__(self)
         self.props.program_name = 'Passes'
-        self.props.version = "0.2.1"
+        self.props.version = "0.3"
         self.props.authors = ['Pablo Sánchez Rodríguez']
         self.props.copyright = '(C) 2022 Pablo Sánchez Rodríguez'
         self.props.logo_icon_name = 'me.sanchezrodriguez.passes'
