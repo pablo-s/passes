dda81b0406771886f9d8a1d01905ba0aa5560d46
Pablo Sánchez Rodríguez <pablo@sanchezrodriguez.me>
2022-04-27 19:14:40 +0200

    Improve barcode drawing
    Barcode widget is now a custom widget instead of a DrawingArea.
    
    The distance between qr-code and the alternative text is now 4
    times the size of the module (recommended margin for QR codes).

--- src/barcode_widget.py
+++ src/barcode_widget.py
@@ -15,15 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import ctypes
-
-from gi.repository import Adw, Gtk
+from gi.repository import Adw, Gdk, Graphene, Gtk
 
 from .barcode_content_encoder import BarcodeContentEncoder
 
 
-@Gtk.Template(resource_path='/me/sanchezrodriguez/passes/barcode_widget.ui')
-class BarcodeWidget(Gtk.DrawingArea):
+class BarcodeWidget(Gtk.Widget):
 
     __gtype_name__ = 'BarcodeWidget'
 
@@ -37,14 +34,20 @@ class BarcodeWidget(Gtk.DrawingArea):
         self.__data_width = 0
         self.__data_height = 0
 
+        # Set foreground color to black
+        self.__foreground_color = Gdk.RGBA()
+        self.__foreground_color.red = 0.0
+        self.__foreground_color.blue = 0.0
+        self.__foreground_color.green = 0.0
+        self.__foreground_color.alpha = 1.0
+
         # Amount of pixels used to draw each of the dots
         # that compound the QR-code
         self.__dot_size = 4
 
-        self.set_draw_func(self._update_drawing_area)
-        self.set_content_height(self.__data_height * self.__dot_size)
+    def do_snapshot(self, snapshot):
+        canvas_width = self.get_allocated_width()
 
-    def _update_drawing_area(self, drawing_area, cairo_context, canvas_width, canvas_height):
         # Center the qr-code horizontally in the canvas
         offset_x = (canvas_width // 2) - (self.__data_width * self.__dot_size // 2)
 
@@ -57,11 +60,14 @@ class BarcodeWidget(Gtk.DrawingArea):
                 if not is_foreground:
                     continue
 
-                cairo_context.fill()
-                cairo_context.rectangle(i * self.__dot_size + offset_x,
-                                        j * self.__dot_size,
-                                        self.__dot_size,
-                                        self.__dot_size)
+                x = i * self.__dot_size + offset_x
+                y = j * self.__dot_size
+                w = self.__dot_size
+                h = self.__dot_size
+
+                rectangle = Graphene.Rect()
+                rectangle.init(x, y, w, h)
+                snapshot.append_color(self.__foreground_color, rectangle)
 
     def message(self, message, encoding):
 
@@ -72,4 +78,6 @@ class BarcodeWidget(Gtk.DrawingArea):
         self.__data_width = side
         self.__data_height = side
 
-        self.set_content_height(self.__data_height * self.__dot_size)
+        barcode_height = self.__data_height * self.__dot_size
+        bottom_margin = 4 * self.__dot_size
+        self.props.height_request = barcode_height + bottom_margin
